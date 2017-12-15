5a99cbde216a2c693afa507559b87a5be03bdd14
FranciscoMH <fmelendezherize@gmail.com>
2017-12-15 12:53:00 -0400

    Algunas mejoras y Swagger

--- professors/models.py
+++ professors/models.py
@@ -1,12 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+
 from courses.models import Subject
-from authentication.models import Profile, ProfileManager
+from authentication.models import Profile
 
 # Create your models here.
+class ProfessorManager(models.Manager):
+    '''
+    Custom Professor Manager
+    '''
+    def create_professor(self, email, password):
+        '''
+        This action is made by the admin user. Create Professor with a mininum data.
+        '''
+        user = Profile.objects.create_user(email=email, password=password)
+        user.user_type = 'P'
+        user.save()
+        #should return professor
+        return user
+
+    def register_professor(self, email, password, **kwargs):
+        '''
+        This action is made by the professor user. Complete the profile data.
+        '''
+        pass
+
 class Professor(models.Model):
+    '''
+    Professor Class
+    '''
     identification_number = models.CharField(max_length=20, null=True, blank=True)
     phone = models.CharField(max_length=200, null=True, blank=True)
     address = models.CharField(max_length=200, null=True, blank=True)
@@ -15,22 +38,7 @@ class Professor(models.Model):
     user = models.OneToOneField(Profile, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=False)
 
+    objects = ProfessorManager()
+
     def __unicode__(self):
         return self.names + " " + str(self.lastnames)
-
-    @staticmethod
-    def create_professor(email, password):
-        '''
-        This action is made by the admin user. Create Professor with a mininum data.
-        '''
-        user = Profile.objects.create_user(email=email, password=password)
-        user.user_type = 'P'
-        user.save()
-    
-    def register_professor(email, password, **kwargs):
-        '''
-        This action is made by the professor user. Complete the profile data.
-        '''
-
-        pass
-    
--- professors/tests.py
+++ professors/tests.py
@@ -2,19 +2,25 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
-from models import Professor
-import courses
+from django.db import IntegrityError
+from .models import Professor
 
 # Create your tests here.
 class ProfessorTestCase(TestCase):
-
+    '''
+    Test Case for Professor
+    '''
     def test_create_professor(self):
-        my_professor = Professor.create_professor(email='fmelendezherize@gmail.com', password='Pikachu32.')
+        my_professor = Professor.objects.create_professor(
+            email='fmelendezherize@gmail.com', password='Pikachu32.')
         self.assertNotEqual(my_professor, None)
 
     def test_create_duplicate_professor(self):
-        my_professor = Professor.create_professor(email='fmelendezherize@gmail.com', password='Pikachu32.')
-        print my_professor
-
-        my_professor = Professor.create_professor(email='fmelendezherize@gmail.com', password='Pikachu12.')
-        print my_professor
+        try:
+            Professor.objects.create_professor(
+                email='fmelendezherize@gmail.com', password='Pikachu32.')
+            Professor.objects.create_professor(
+                email='fmelendezherize@gmail.com', password='Pikachu12.')
+            self.fail("Not Duplicate Professors Validation Fail")
+        except IntegrityError:
+            pass
